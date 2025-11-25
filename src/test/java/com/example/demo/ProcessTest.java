b40dad6dbf74298991e081b0936567e61a2d1feb
Philipp Ossler <philipp.ossler@gmail.com>
2025-11-25 11:42:12 +0100

    fix: User task completion in process test
    There are two issues in the process test: 
    
    1) Bug: The user task completion doesn't filter by the user task state. As a result, it tries to complete an already completed task. 
    
    2) Design: The completion uses the user task search API which is eventually consistent. That mean there is a delay until the user task state is updated by the completion. To handle the consistency, we could wait the user task is completed and left.

--- src/test/java/com/example/demo/ProcessTest.java
+++ src/test/java/com/example/demo/ProcessTest.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
 import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.client.api.search.enums.UserTaskState;
+import io.camunda.client.api.search.filter.UserTaskFilter;
+import io.camunda.client.api.search.response.UserTask;
+import io.camunda.process.test.api.CamundaAssert;
 import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
+import io.camunda.process.test.api.assertions.UserTaskSelector;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
+
+import static io.camunda.process.test.api.assertions.ElementSelectors.byId;
 import static io.camunda.process.test.api.assertions.UserTaskSelectors.byElementId;
 
 @SpringBootTest
@@ -21,7 +30,7 @@ public class ProcessTest {
 
     @Test
     void test() {
-        client.newCreateInstanceCommand()
+        final ProcessInstanceEvent processInstance = client.newCreateInstanceCommand()
                 .bpmnProcessId("Process_DoIt")
                 .latestVersion()
                 .send()
@@ -30,7 +39,50 @@ public class ProcessTest {
         processTestContext.completeUserTask(byElementId("UserTask_DoIt"));
         System.err.println("completed task: 1");
 
+        // -------------------------------
+
+        // 1) Eventually consistency: wait until the previous user task completion is visible
+        CamundaAssert.assertThat(processInstance).hasCompletedElement(byId("UserTask_DoIt"), 1);
+
         // following statement fails
-        processTestContext.completeUserTask(byElementId("UserTask_DoIt"));
+        // 2) Workaround: use a custom selector to filter only created/active user tasks
+        processTestContext.completeUserTask(byElementId("UserTask_DoIt").and(
+                userTask -> UserTaskState.CREATED.equals(userTask.getState())
+        ));
+        System.err.println("completed task: 2");
+
+        // -------------------------------
+
+        CamundaAssert.assertThat(processInstance).hasCompletedElement(byId("UserTask_DoIt"), 2);
+
+        // 3) Alternative workaround: extract the custom selector implementation
+        processTestContext.completeUserTask(byElementId("UserTask_DoIt").and(created()));
+
+        System.err.println("completed task: 3");
+
+        // -------------------------------
+
+        CamundaAssert.assertThat(processInstance).hasCompletedElement(byId("UserTask_DoIt"), 3);
+    }
+
+    private UserTaskSelector created() {
+        return new CreatedUserTaskSelector();
+    }
+
+    private static final class CreatedUserTaskSelector implements UserTaskSelector {
+        @Override
+        public boolean test(UserTask userTask) {
+            return UserTaskState.CREATED.equals(userTask.getState());
+        }
+
+        @Override
+        public void applyFilter(UserTaskFilter filter) {
+            filter.state(UserTaskState.CREATED);
+        }
+
+        @Override
+        public String describe() {
+            return "is created";
+        }
     }
 }
